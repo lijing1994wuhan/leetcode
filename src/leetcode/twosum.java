dafed6682be795da5dfea6cd64b82c42cd79bfa2
lijing1994wuhan <jli108@stevens.edu>
2018-02-05 19:32:59 -0500

    2sum(3 solution)

--- src/leetcode/twosum.java
+++ src/leetcode/twosum.java
@@ -5,16 +5,17 @@ import java.util.HashMap;
 
 public class twosum {
 	public int[] twoSum(int[] nums, int target) {
-//	    for (int i = 0; i < nums.length; i++) {
-//	        for (int j = i + 1; j < nums.length; j++) {
-//	            if (nums[j] == target - nums[i]) {
-//	                return new int[] { i, j };
-//	            }
-//	        }
-//	    }
-//	    throw new IllegalArgumentException("No two sum solution");
+	   /* for (int i = 0; i < nums.length; i++) {
+	        for (int j = i + 1; j < nums.length; j++) {
+	            if (nums[j] == target - nums[i]) {
+	                return new int[] { i, j };
+	            }
+	        }
+	    }
+	    throw new IllegalArgumentException("No two sum solution");
+	  1 solution  */
 		
-		    HashMap<Integer, Integer> map = new HashMap<>();
+		   /* HashMap<Integer, Integer> map = new HashMap<>();
 		    for (int i = 0; i < nums.length; i++) {
 		        map.put(nums[i], i);
 		    }
@@ -23,6 +24,16 @@ public class twosum {
 		        if (map.containsKey(complement) && map.get(complement) != i) {
 		            return new int[] { i, map.get(complement) };
 		        }
+		    }
+		    throw new IllegalArgumentException("No two sum solution");
+		    2 solution */
+		 HashMap<Integer, Integer> map = new HashMap<>();
+		    for (int i = 0; i < nums.length; i++) {
+		        int complement = target - nums[i];
+		        if (map.containsKey(complement)) {
+		            return new int[] { map.get(complement), i };
+		        }
+		        map.put(nums[i], i);
 		    }
 		    throw new IllegalArgumentException("No two sum solution");
 		}
