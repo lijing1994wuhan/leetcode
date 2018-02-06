75c6343495d91e71d0b9937513c218b41a8e13ec
lijing1994wuhan <jli108@stevens.edu>
2018-02-05 19:28:19 -0500

    2sum

---
+++ src/leetcode/twosum.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class twosum {
+	public int[] twoSum(int[] nums, int target) {
+//	    for (int i = 0; i < nums.length; i++) {
+//	        for (int j = i + 1; j < nums.length; j++) {
+//	            if (nums[j] == target - nums[i]) {
+//	                return new int[] { i, j };
+//	            }
+//	        }
+//	    }
+//	    throw new IllegalArgumentException("No two sum solution");
+		
+		    HashMap<Integer, Integer> map = new HashMap<>();
+		    for (int i = 0; i < nums.length; i++) {
+		        map.put(nums[i], i);
+		    }
+		    for (int i = 0; i < nums.length; i++) {
+		        int complement = target - nums[i];
+		        if (map.containsKey(complement) && map.get(complement) != i) {
+		            return new int[] { i, map.get(complement) };
+		        }
+		    }
+		    throw new IllegalArgumentException("No two sum solution");
+		}
+		
+	
+	
+	public static void main(String[]args) {
+		twosum a = new twosum();
+		int[] b=new int[] {2,3,4};
+		int[] c=new int[] {};
+			c=a.twoSum(b,6);
+		System.out.println(Arrays.toString(c));
+	}
+}
